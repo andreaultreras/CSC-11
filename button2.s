f6cb51855de0d55e54e171e3e947187590548206
andreaultreras <57914531+andreaultreras@users.noreply.github.com>
2019-12-02 09:25:59 -0800

    Update button2.s

--- button2.s
+++ button2.s
@@ -1,13 +1,16 @@
+//This program makes the LED flash 5 times, if the button is pressed when the LED is lit is stays on for 5 seconds
+//then ends the program.
 .equ INPUT,0
 .equ OUTPUT, 1
 .equ LOW, 0
 .equ HIGH, 1
 .equ PIN, 26
-.equ LED_PIN, 29
+.equ LED_PIN, 29	//button
 
 .text
 .global main
 main:
+	//sets up the LED and button
 	push {lr}
 	bl wiringPiSetup
 
@@ -67,6 +70,7 @@ then:
 	beq end_while_loop
 
 end_while_loop:
+	//keeps the LED on
 	mov r0, #PIN
 	mov r1, #HIGH
 	bl digitalWrite
@@ -82,24 +86,3 @@ end_program:
 
 	mov r0, #0
 	pop {pc}
-
-/*mov r0, #LED_PIN
-bl digitalRead
-mov r1, #PIN
-cmp r0, r1
-bne increment
-beq end_while_loop*/
-
-/*things to get done by wed:
-	1. get LED to stay on if button is pressed when light is on
-		a. are if statements possible? am i building it correctly?
-		   how to cmp pins and buttons?
-		b. ideas: do, blinking while button = low
-		c. compare pin voltage to button voltage if equal LED stay on
-		d. if not branch to loop or end
-	2. get LED to keep blinking if button is pressed when LED is off 
-	   or get program to end
-	3. add a second LED
-	4. get button to turn on second LED if pressed when 1st LED is on
-	5. repeat number 2
-*/
